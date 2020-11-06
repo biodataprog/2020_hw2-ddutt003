c4d70af1cd632a26c754a5741233d1bd8b4b57f1
ddutt003 <73002609+ddutt003@users.noreply.github.com>
2020-11-06 10:44:40 -0800

    Update sevenless.py

--- sevenless.py
+++ sevenless.py
@@ -5,3 +5,6 @@ start = 0
 end   = 99
 divisor=7
 print("Print out all numbers from",start,"to",end, "that are not divisible by",divisor)
+for solution in range(0, 99):
+  if solution % 7 != 0:
+    print(solution)
