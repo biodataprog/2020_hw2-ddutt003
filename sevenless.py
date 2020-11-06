efdf8c22113db126b9c22f0a1a2324b4ff82c2bc
ddutt003 <73002609+ddutt003@users.noreply.github.com>
2020-11-06 09:43:07 -0800

    Update sevenless.py

--- sevenless.py
+++ sevenless.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
+
 start = 0
 end   = 99
 divisor=7
-print("Printing out numbers from",start,"to",end, " not divisible by",divisor)
+print("Print out all numbers from",start,"to",end, "that are not divisible by",divisor)
